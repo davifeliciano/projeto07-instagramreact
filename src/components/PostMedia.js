ec7597d1fe8966ed254096fb55871b55801a4bd3
Davi Feliciano <dfeliciano37@gmail.com>
2023-02-11 16:01:26 -0300

    Adicionando animação de like

--- src/components/PostMedia.js
+++ src/components/PostMedia.js
@@ -1,22 +1,16 @@
+import { useState } from "react";
+
 export default function PostMedia(props) {
   const { like, likeSetter, likeAmount, likeAmountSetter } = props;
+  const [animationVisibility, setAnimationVisibility] = useState(false);
+  const visibilityDuration = 1000;
 
   function likePost() {
     if (like) return;
     likeSetter(true);
     likeAmountSetter(likeAmount + 1);
-  }
-
-  if (props.type === "image") {
-    return (
-      <img
-        src={props.src}
-        alt={props.description}
-        width="1500"
-        height="1500"
-        onDoubleClick={likePost}
-      />
-    );
+    setAnimationVisibility(true);
+    setTimeout(() => setAnimationVisibility(false), visibilityDuration);
   }
 
   function getVideoType(src) {
@@ -24,19 +18,46 @@ export default function PostMedia(props) {
     return `video/${extension}`;
   }
 
+  if (props.type === "image") {
+    return (
+      <div className="post-media">
+        <img
+          className={`like-icon ${animationVisibility ? "" : "hidden"}`}
+          src="assets/heart.svg"
+          alt="Like Icon"
+          onClick={() => setAnimationVisibility(false)}
+        />
+        <img
+          src={props.src}
+          alt={props.description}
+          width="1500"
+          height="1500"
+          onDoubleClick={likePost}
+        />
+      </div>
+    );
+  }
+
   return (
-    <video
-      loop
-      autoPlay
-      muted
-      width="1080"
-      height="1080"
-      onDoubleClick={likePost}
-    >
-      {props.src.map((src, index) => {
-        return <source key={index} src={src} type={getVideoType(src)} />;
-      })}
-      Seu navegador não suporta vídeos
-    </video>
+    <div className="post-media">
+      <img
+        className={`like-icon ${animationVisibility ? "" : "hidden"}`}
+        src="assets/heart.svg"
+        alt="Like Icon"
+      />
+      <video
+        loop
+        autoPlay
+        muted
+        width="1080"
+        height="1080"
+        onDoubleClick={likePost}
+      >
+        {props.src.map((src, index) => {
+          return <source key={index} src={src} type={getVideoType(src)} />;
+        })}
+        Seu navegador não suporta vídeos
+      </video>
+    </div>
   );
 }
