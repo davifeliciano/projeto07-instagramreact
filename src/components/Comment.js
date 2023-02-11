68b64a2287c72a59065a539e6b1befb7c2098c28
Davi Feliciano <dfeliciano37@gmail.com>
2023-02-11 16:21:34 -0300

    Adicionando feature de like nos Comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function Comment(props) {
+  const [like, setLike] = useState(false);
+
+  function toggleLike() {
+    setLike(!like);
+  }
+
   return (
     <div className="comment">
       <p>
@@ -8,9 +16,12 @@ export default function Comment(props) {
         {props.content}
       </p>
       <div className="nav-items">
-        <a href="/#">
-          <ion-icon name="heart-outline"></ion-icon>
-        </a>
+        <button
+          className={`like-btn ${like ? "liked" : ""}`}
+          onClick={toggleLike}
+        >
+          <ion-icon name={`heart${like ? "" : "-outline"}`}></ion-icon>
+        </button>
       </div>
     </div>
   );
